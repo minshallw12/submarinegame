c95e008ffa082b8eb7dc33494edf30733c1f797e
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-10-14 20:28:01 -0600

    added destroyers and depth chargers

--- submarine.py
+++ submarine.py
@@ -26,7 +26,6 @@ SAND_HEIGHT = 60
 # Define the height of the sky
 SKY_HEIGHT = 100
 
-
 # Define a Player object by extending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player'
 class Sand:
@@ -87,9 +86,9 @@ class Torpedo(pygame.sprite.Sprite):
 
 # Define the enemy object by extending pygame.sprite.Sprite
 # The surface you draw on the screen is now an attribute of 'enemy'
-class Enemy(pygame.sprite.Sprite):
+class EnemyTorp(pygame.sprite.Sprite):
     def __init__(self):
-        super(Enemy, self).__init__()
+        super(EnemyTorp, self).__init__()
         self.surf = pygame.Surface((20,10))
         self.surf.fill((0, 0, 0))
         self.rect = self.surf.get_rect(
@@ -105,6 +104,37 @@ class Enemy(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+class Destroyer(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Destroyer, self).__init__()
+        self.surf = pygame.Surface((50,20))
+        self.surf.fill((0, 0, 0))
+        self.rect = self.surf.get_rect(
+            center = (
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                95
+            )
+        )
+        self.speed = random.randint(1, 3)
+        self.last_drop_time = 0
+    
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+class Depth_Charge(pygame.sprite.Sprite):
+    def __init__(self, initial_position):
+        super(Depth_Charge, self).__init__()
+        self.surf = pygame.Surface((10,10))
+        self.surf.fill((50, 10, 200))
+        self.rect = self.surf.get_rect(center = initial_position)
+        self.speed = random.randint(1,3)
+    
+    def update(self):
+        self.rect.move_ip(0, self.speed)
+        if self.rect.top > 940:
+            self.kill()
 
 # Initialize pygame
 pygame.init()
@@ -114,8 +144,13 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Create a custom event for adding a new enemy
-ADDENEMY = pygame.USEREVENT + 1
-pygame.time.set_timer(ADDENEMY, 500)
+ADDENEMYTORP = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMYTORP, 500)
+ADDDESTROYER = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDDESTROYER, 7000)
+ADDDEPTH_CHARGE = pygame.USEREVENT + 3
+pygame.time.set_timer(ADDDEPTH_CHARGE, random.randint(500, 3000))
+
 
 # Instantiate the player, sky, and sand
 player = Player()
@@ -125,10 +160,14 @@ sand = Sand()
 # Create groups to hold enemy sprites and all sprites
 # - enemies is used for collision detection and position updates
 # - all_sprites is used for rendering
-enemies = pygame.sprite.Group()
+enemy_torps = pygame.sprite.Group()
+destroyers = pygame.sprite.Group()
+depth_charges = pygame.sprite.Group()
 torpedos = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
+
+destroyer_delays = {}
 
 #Variable to keep main loop running
 running = True
@@ -156,12 +195,28 @@ while running:
         elif event.type == QUIT:
             running = False
 
-        # Add a new enemey to game
-        elif event.type == ADDENEMY:
+        # Add a new enemy to game
+        elif event.type == ADDENEMYTORP:
             # Create a new enemy and add it to sprite group
-            new_enemy = Enemy()
-            enemies.add(new_enemy)
-            all_sprites.add(new_enemy)
+            new_enemy_torp = EnemyTorp()
+            enemy_torps.add(new_enemy_torp)
+            all_sprites.add(new_enemy_torp)
+        
+        elif event.type == ADDDESTROYER:
+            new_destroyer = Destroyer()
+            destroyers.add(new_destroyer)
+            all_sprites.add(new_destroyer)
+            destroyer_delays[new_destroyer] = random.randint(500, 3000)
+
+        elif event.type == ADDDEPTH_CHARGE:
+            for destroyer in destroyers:
+                if destroyer in destroyer_delays and pygame.time.get_ticks() - destroyer.last_drop_time >= destroyer_delays[destroyer]:
+                    new_depth_charge = Depth_Charge(destroyer.rect.center)
+                    depth_charges.add(new_depth_charge)
+                    all_sprites.add(new_depth_charge)
+                    # Update the last drop time for the destroyer
+                    destroyer.last_drop_time = pygame.time.get_ticks()
+
         
 
     # Get the set of keys pressed and check for user input
@@ -171,7 +226,9 @@ while running:
     player.update(pressed_keys)
 
     # Update enemey position each frame
-    enemies.update()
+    enemy_torps.update()
+    destroyers.update()
+    depth_charges.update()
     torpedos.update()
 
     # Fill the screen with color
@@ -186,18 +243,23 @@ while running:
         screen.blit(sprite.surf, sprite.rect)
 
     # Check if an enemy collides with player
-    if pygame.sprite.spritecollideany(player, enemies):
+    if pygame.sprite.spritecollideany(player, enemy_torps) or pygame.sprite.spritecollideany(player, destroyers):
         # If so, remove the player and stop the game loop
         player.kill()
         running = False
 
     # Check if a torpedo collides with an enemy. Groupcollide() is used for collisions between sprite groups
-    collisions = pygame.sprite.groupcollide(torpedos, enemies, True, True)
+    enemy_torp_collisions = pygame.sprite.groupcollide(torpedos, enemy_torps, True, True)
+    destroyer_collisions = pygame.sprite.groupcollide(torpedos, destroyers, True, True)
 
     # If there are collisions, remove the torpedo and enemy
-    for torpedo, enemy in collisions.items():
+    for torpedo, enemy_torp in enemy_torp_collisions.items():
         torpedo.kill()
-        for e in enemy:
+        for e in enemy_torp:
+            e.kill()
+    for torpedo, destroyer in destroyer_collisions.items():
+        torpedo.kill()
+        for e in destroyer:
             e.kill()
 
     # Update the display
